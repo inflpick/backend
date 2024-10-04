8956a2505c1252d18748a952bffb594cfd50ca0d
leesh5000 <leesh50000@gmail.com>
2024-10-04 13:57:52 +0900

    #15 - CodeDeploy 테스트

--- infra/scripts/application_start.sh
+++ infra/scripts/application_start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Load variables from .env file
-if [ -f .env ]; then
-  source .env
+if [ -f /root/app/.env ]; then
+  source /root/app/.env
 fi
 
 # Docker login using environment variables from .env
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_TOKEN
 docker-compose pull
-docker-compose --env-file .env up -d
+docker-compose --env-file /root/app/.env up -d
