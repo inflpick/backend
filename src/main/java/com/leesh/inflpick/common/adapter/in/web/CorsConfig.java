79d3b6d8c63af5a6410e5eab092fc7bdd147e3eb
leesh5000 <leesh50000@gmail.com>
2024-10-09 18:30:37 +0900

    #43 - Cors 설정 추가

---
+++ src/main/java/com/leesh/inflpick/common/adapter/in/web/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.leesh.inflpick.common.adapter.in.web;
+
+import com.leesh.inflpick.common.adapter.in.web.value.CorsProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@EnableConfigurationProperties({CorsProperties.class})
+@RequiredArgsConstructor
+@Configuration
+public class CorsConfig {
+
+    private final CorsProperties corsProperties;
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(corsProperties.allowedOrigins()));
+        config.setAllowedMethods(Arrays.asList(corsProperties.allowedMethods().split(",")));
+        config.setAllowedHeaders(List.of(corsProperties.allowedHeaders()));
+        config.setAllowCredentials(corsProperties.allowCredentials());
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
