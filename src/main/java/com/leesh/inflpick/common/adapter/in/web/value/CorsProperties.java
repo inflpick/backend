0b19437902d49a2662c74984dd2f8ae7ac95f0d3
leesh5000 <leesh50000@gmail.com>
2024-10-09 19:22:13 +0900

    #45 - CORS 403 버그 수정

--- src/main/java/com/leesh/inflpick/common/adapter/in/web/value/CorsProperties.java
+++ src/main/java/com/leesh/inflpick/common/adapter/in/web/value/CorsProperties.java
@@ -3,5 +3,5 @@ package com.leesh.inflpick.common.adapter.in.web.value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "custom.cors")
-public record CorsProperties(String allowedOrigins, String allowedMethods, String allowedHeaders, boolean allowCredentials) {
+public record CorsProperties(String allowedOriginPatterns, String allowedMethods, String allowedHeaders, boolean allowCredentials) {
 }
