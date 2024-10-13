02c4e14f8a358bd59962eef6c8d290709fb4391f
leesh5000 <leesh50000@gmail.com>
2024-10-13 09:37:09 +0900

    #59 - 로그인 없이 해당 URL에 접근하면 에러메세지를 보내도록 수정

---
+++ src/main/java/com/leesh/inflpick/common/adapter/in/web/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.leesh.inflpick.common.adapter.in.web.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
