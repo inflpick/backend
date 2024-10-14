db98fde4ddd873a8edd519f7e74e5426f3c89a63
leesh5000 <leesh5000@aerix.co.kr>
2024-10-14 10:24:04 +0900

    #68 - favicon.ico 를 찾을 수 없는 에러 로그 해결

--- src/main/java/com/leesh/inflpick/common/adapter/in/web/WebConfig.java
+++ src/main/java/com/leesh/inflpick/common/adapter/in/web/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
-                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/resources/**", "/favicon.ico")
+                .addResourceLocations("classpath:/static/", "classpath:/static/**");
     }
 }
