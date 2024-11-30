46aace4f4e837bf0afa157f345e1488a7e852a6a
leesh5000 <leesh50000@gmail.com>
2024-11-30 17:07:24 +0900

    사용하지 않는 파일 제거

--- src/test/java/com/leesh/inflpick/v2/adapter/out/persistence/mongo/user/UserDocumentTest.java
+++
@@ -1,9 +0,0 @@
-package com.leesh.inflpick.v2.adapter.out.persistence.mongo.user;
-
-import org.junit.jupiter.api.DisplayName;
-
-@DisplayName("어댑터 테스트 : UserDocument")
-public class UserDocumentTest {
-
-
-}
--- src/test/java/com/leesh/inflpick/v2/application/service/user/QueryUserServiceTest.java
+++
@@ -1,7 +0,0 @@
-package com.leesh.inflpick.v2.application.service.user;
-
-class QueryUserServiceTest {
-
-
-
-}
